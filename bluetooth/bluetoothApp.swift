a741e701c589aaa52303282cfa90ad89f0817d61
Ryan Helgeson <ryanhelgeson14@gmail.com>
2024-01-16 14:11:09 -0500

    Initial Commit

---
+++ bluetooth/bluetoothApp.swift
@@ -0,0 +1,17 @@
+//
+//  bluetoothApp.swift
+//  bluetooth
+//
+//  Created by Ryan Helgeson on 1/16/24.
+//
+
+import SwiftUI
+
+@main
+struct bluetoothApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
